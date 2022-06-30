d019e048950a4802af204ebe146c43eebcb25a3e
ojasvikaushal <88622651+ojasvikaushal@users.noreply.github.com>
2022-06-30 19:13:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+prediction = "";
+
+Webcam.set({
+    width :350,
+    height:300,
+    image_format :'png',
+    png_quality :90
+});
+camera = document.getElementById("camera");
+
+Webcam.attach(camera);
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "'+data_uri+'">';
+    });
+}
+
+console.log("ml5 version : ", ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/prVly5KJc/model.json', modelLoaded);
+
+function modelLoaded()
+{
+    console.log("model loaded successfully");
+    speak();
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data = "The Prediction is "+ prediction;
+    utter_this = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utter_this);
+}
